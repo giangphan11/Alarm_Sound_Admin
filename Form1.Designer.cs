73e4a3bf28ab575fbe0603927be224b40be4d715
giangphan11 <giangphan912@gmail.com>
2023-08-18 22:08:52 +0700

    init project

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace SoundClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIpAddress = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbSendAll = new System.Windows.Forms.CheckBox();
+            this.flAddress = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(16, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play sound";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.playSound_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(379, 16);
+            this.button2.Margin = new System.Windows.Forms.Padding(16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 68);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop sound";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.stopSound_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(200, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Admin";
+            // 
+            // txtIpAddress
+            // 
+            this.txtIpAddress.AutoSize = true;
+            this.txtIpAddress.Location = new System.Drawing.Point(433, 60);
+            this.txtIpAddress.Name = "txtIpAddress";
+            this.txtIpAddress.Size = new System.Drawing.Size(46, 17);
+            this.txtIpAddress.TabIndex = 4;
+            this.txtIpAddress.Text = "label3";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 453);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(16);
+            this.panel1.Size = new System.Drawing.Size(582, 100);
+            this.panel1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbSendAll);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(0, 353);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(582, 100);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Điều khiển";
+            // 
+            // cbSendAll
+            // 
+            this.cbSendAll.AutoSize = true;
+            this.cbSendAll.Location = new System.Drawing.Point(13, 21);
+            this.cbSendAll.Name = "cbSendAll";
+            this.cbSendAll.Size = new System.Drawing.Size(91, 21);
+            this.cbSendAll.TabIndex = 1;
+            this.cbSendAll.Text = "Gửi tất cả";
+            this.cbSendAll.UseVisualStyleBackColor = true;
+            this.cbSendAll.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // flAddress
+            // 
+            this.flAddress.Location = new System.Drawing.Point(0, 133);
+            this.flAddress.Name = "flAddress";
+            this.flAddress.Size = new System.Drawing.Size(570, 201);
+            this.flAddress.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 553);
+            this.Controls.Add(this.flAddress);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtIpAddress);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label txtIpAddress;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox cbSendAll;
+        private System.Windows.Forms.FlowLayoutPanel flAddress;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SoundClient
+{
+    public partial class Form1 : Form
+    {
+        private UdpClient udpClient;
+        private IPEndPoint endPoint;
+
+        private static int port = 12345;
+        public Form1()
+        {
+            InitializeComponent();
+            // scanIPDevice();
+
+            udpClient = new UdpClient();
+            endPoint = new IPEndPoint(IPAddress.Broadcast, port);
+            scan2();
+        }
+
+        private void scan2() {
+            try
+            {
+                string hostName = Dns.GetHostName();
+                IPAddress[] ipAddresses = Dns.GetHostAddresses(hostName);
+
+                List<string> ipAddressList = new List<string>();
+
+                foreach (IPAddress ipAddress in ipAddresses)
+                {
+                    if (ipAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        ipAddressList.Add(ipAddress.ToString());
+                        CheckBox cb = new CheckBox();
+                        cb.Text = ipAddress.ToString();
+                        flAddress.Controls.Add(cb);
+                    }
+                }
+
+                txtIpAddress.Text = string.Join(";", ipAddressList);
+                
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occurred while getting IP addresses: " + ex.Message);
+            }
+        }
+
+        private void scanIPDevice() {
+            // Địa chỉ IP mạng LAN của bạn, thay đổi địa chỉ này tùy theo dải mạng của bạn
+            string lanIpBase = "192.168.0.";
+            // int port = port; // Port của ứng dụng đang chạy trên máy tính mục tiêu
+
+            Console.WriteLine("Danh sách máy tính đang kết nối trong cùng một dải mạng LAN:");
+
+            for (int i = 1; i < 255; i++)
+            {
+                string ipAddress = lanIpBase + i.ToString();
+                try
+                {
+                    UdpClient udpClient = new UdpClient();
+                    udpClient.Connect(ipAddress, port);
+
+                    // Gửi gói tin UDP tới địa chỉ IP của máy tính mục tiêu
+                    byte[] sendBytes = new byte[1];
+                    udpClient.Send(sendBytes, sendBytes.Length);
+
+                    // Nhận gói tin UDP từ máy tính mục tiêu (Timeout 100ms)
+                    IPEndPoint remoteIpEndPoint = new IPEndPoint(IPAddress.Any, port);
+                    udpClient.Client.ReceiveTimeout = 100;
+                    byte[] receiveBytes = udpClient.Receive(ref remoteIpEndPoint);
+
+                    // Nếu không có lỗi xảy ra, máy tính mục tiêu đang hoạt động
+                    Console.WriteLine($"Máy tính có địa chỉ IP: {ipAddress} đang hoạt động.");
+
+                    udpClient.Close();
+                }
+                catch (SocketException)
+                {
+                    // Ignored, máy tính không phản hồi hoặc có lỗi
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private void playSound_Click(object sender, EventArgs e)
+        {
+            byte[] data = System.Text.Encoding.ASCII.GetBytes("PlaySound");
+            udpClient.Send(data, data.Length, endPoint);
+        }
+
+        private void stopSound_Click(object sender, EventArgs e)
+        {
+            byte[] data = System.Text.Encoding.ASCII.GetBytes("StopSound");
+            udpClient.Send(data, data.Length, endPoint);
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
